05a3d8dcbf158c5aff3884d760569257bc3de8d2
adrianmarino <adrianmarino@gmail.com>
2022-11-13 12:23:37 -0300

    feat: show points

--- src/plot/comparative_box_plot.py
+++ src/plot/comparative_box_plot.py
@@ -2,7 +2,16 @@ import matplotlib.pyplot as plt
 import seaborn           as sns
 
 
-def comparative_box_plot(df, x, y, figsize=(8, 6), leyend=False):
+def comparative_box_plot(
+    df,
+    x,
+    y,
+    figsize     = (8, 6),
+    leyend      = False, 
+    points      = True, 
+    point_color = 'black',
+    point_alpha = 0.1
+):
     sns.set(rc={'figure.figsize' : figsize})
     sns.boxplot(
         data     = df, 
@@ -13,6 +22,14 @@ def comparative_box_plot(df, x, y, figsize=(8, 6), leyend=False):
         notch    = True, 
         showcaps = False
     )
+    if points:
+        sns.stripplot(
+            data  = df, 
+            x     = x, 
+            y     = y,
+            color = point_color,
+            alpha = point_alpha
+        )
     if not leyend:
         plt.legend([],[], frameon=False)
     plt.show()
